266fe4b7578763637a96045cf0a813376ea6656b
Andrej Katin <katin.andrej96@gmail.com>
2020-11-29 21:51:50 +0100

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3 };
+            permutations(arr, 0, 2);
+            Console.Read();
+        }
+
+        public static void permutations(int[] arr, int i, int length)
+        {
+
+            if (i == length)
+            {
+
+            }
+            else
+            {
+                for (int j = 0; j <= length; j++)
+                {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                    Console.WriteLine(arr[0].ToString() + arr[1].ToString() + arr[2].ToString());
+
+
+                    permutations(arr, i + 1, length);
+                }
+
+
+            }
+
+        }
+    }
+
+}
+
